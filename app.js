e4db5b8adfecdfac351b00a3d2f12aa1abe04aa2
Aryadwito <raryadwitoo@gmail.com>
2025-11-11 17:37:32 +0700

    navbar tpi msh berantakan

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    return res.render("index")
-})
+  return res.render("pages/index");
+});
 
 app.listen(port, () => {
-    console.log(`App is running on port: `,port)
-})
+  console.log(`App is running on port: `, port);
+});
