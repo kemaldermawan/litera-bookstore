f2a80a3cd0941fc8b92860abac6f55622f9c01e6
Dayu Aji Priawan <dayupriawan31@gmail.com>
2025-12-13 00:58:09 +0700

    Update Alur login dan registrasi untuk cantumin email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,19 @@
 const User = require('../models/User');
 
 exports.getLogin = (req, res) => {
-    res.render('pages/login');
+    const deleted = req.query.deleted || undefined;
+    const loggedOut = req.query.logged_out || undefined;
+    res.render('pages/login', { deleted, loggedOut });
 };
 
 exports.postLogin = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user) return res.render('pages/login', { error: 'User tidak ditemukan' });
+    if (!user) return res.render('pages/login', { error: 'User tidak ditemukan', email });
 
     const isMatch = await user.comparePassword(password);
-    if (!isMatch) return res.render('pages/login', { error: 'Password salah' });
+    if (!isMatch) return res.render('pages/login', { error: 'Password salah', email });
 
     req.session.user = {
         id: user._id,
@@ -22,11 +24,13 @@ exports.postLogin = async (req, res) => {
         address: user.address
     };
 
+    req.session.success = `Selamat datang kembali, ${user.username}!`;
+
     if (user.role === 'admin') {
         return res.redirect('/admin/books');
     }
 
-    res.redirect('/');
+    res.redirect('/store');
 };
 
 exports.getRegister = (req, res) => {
@@ -49,7 +53,8 @@ exports.postRegister = async (req, res) => {
             address: newUser.address
         };
 
-        res.redirect('/');
+        req.session.success = `Salam hangat, pengunjung baru 👐😇`;
+        res.redirect('/store');
     } catch (err) {
         res.render('pages/register', { error: 'Email atau username sudah terpakai' });
     }
@@ -57,6 +62,6 @@ exports.postRegister = async (req, res) => {
 
 exports.logout = (req, res) => {
     req.session.destroy(() => {
-        res.redirect('/');
+        res.redirect('/login?logged_out=true');
     });
 };
